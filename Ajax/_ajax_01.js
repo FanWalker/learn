4420d2752844bb480ff26d7fd5f5620e796d1d84
fanwalker <fanweijun2@gmail.com>
2017-09-02 20:16:07 +0800

    Create _ajax_01.js

---
+++ Ajax/_ajax_01.js
@@ -0,0 +1,6 @@
+/*
+ *AJAX:Asynchronous JavaScript And Xml, JavaScript执行异步网络请求
+ *用户点击“Submit”按钮，表单开始提交，浏览器就会刷新页面，web的运作原理：一次HTTP请求对应一个页面
+ *要让用户留在当前页面，同时发送请求，要用JavaScript实现，接收到数据后，再用JavaScript更新页面
+ */
+//实现AJAX主要依靠XMLHttpRequest对象：
